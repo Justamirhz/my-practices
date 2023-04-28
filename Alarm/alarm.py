3cd2ce5c9e48aca1bb45eeef901bf106cdc9e5e6
Justamirhz <127483113+Justamirhz@users.noreply.github.com>
2023-04-28 14:27:17 -0700

    just for seconds
    در اینجا من برای اضافه کردن دقیقه و ساعت give up می‌کنم و فقط به ثانیه بسنده می‌کنم.
    ولی بعدا یک آلارم پیشرفته‌تر می‌نویسم

--- Alarm/alarm.py
+++ Alarm/alarm.py
@@ -1,9 +1,5 @@
-
 import datetime
 import time
-
-# which = input("hour or minite or sec?")
-# print("1-hour \n 2-minite \n 3-sec")
 
 
 
@@ -15,17 +11,9 @@ final_s = now.second+user_time
 
 
 while True:
-    current_m = datetime.datetime.now().minute
-    current_h = datetime.datetime.now().hour
     current_s = datetime.datetime.now().second
-
-
-    # print(current_h)
-    # print(current_m)
-    # print(current_s)
     time.sleep(1) 
     if final_s<=current_s:
      print("sound!")
      exit()
-
 
